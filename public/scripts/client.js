53fa3a119a313a1bd72b09a60d9e902671844e6f
Edison Yang <edison.cy.yang@gmail.com>
2019-12-18 06:24:17 +0000

    ajax call to get all the tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,17 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 const createTweetElement = function(tweet) {
-//   const test = {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//       },
-//     "content": {
-//         "text": "If I have seen further it is by standing on the shoulders of giants"
-//       },
-//     "created_at": 1461116232227
-//  };
  const tweetElement = `
   <article class="tweet">
     <header>
@@ -48,6 +37,27 @@ const renderTweets = function(tweets) {
     $('.tweet-container').append($tweet);
   }
 }
+
+const createTweet = function() {
+  const form = $('.new-tweet form');
+  form.on('submit', function(event) {
+    event.preventDefault();
+    const inputData = $(this).serialize();
+    $.ajax('/tweets/', {
+      method: 'POST',
+      data: inputData
+    })
+    .then(function(data) {
+    });
+  });
+};
+
+const loadTweets = function() {
+  $.ajax('/tweets/', { method: 'GET' })
+  .then(function(data) {
+    renderTweets(data);
+  });
+};
 
 $(document).ready(function() {
   const tweetData = [
@@ -86,18 +96,6 @@ $(document).ready(function() {
   ]
   
   renderTweets(tweetData);
-
-  const form = $('.new-tweet form');
-  form.on('submit', function(event) {
-    console.log('submitted...performing ajax call');
-    event.preventDefault();
-    const inputData = $(this).serialize();
-    $.ajax('/tweets/', {
-      method: 'POST',
-      data: inputData
-    })
-    .then(function(data) {
-      console.log(data);
-    });
-  });
+  loadTweets();
+  createTweet();
 });
