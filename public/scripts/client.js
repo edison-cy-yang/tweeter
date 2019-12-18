1188f1a4bd5865554aa15ce47ae637b6d86b1488
Edison Yang <edison.cy.yang@gmail.com>
2019-12-18 06:55:45 +0000

    render the newly added tweet upon submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -34,7 +34,7 @@ const createTweetElement = function(tweet) {
 const renderTweets = function(tweets) {
   for (let tweet of tweets) {
     const $tweet = createTweetElement(tweet);
-    $('.tweet-container').append($tweet);
+    $('.tweet-container').prepend($tweet);
   }
 }
 
@@ -42,12 +42,24 @@ const createTweet = function() {
   const form = $('.new-tweet form');
   form.on('submit', function(event) {
     event.preventDefault();
+    const tweetContent = $('.new-tweet textarea');
+    const length = tweetContent.val().length;
+    if (tweetContent.val() === "") {
+      alert("Your tweet is empty!");
+      return;
+    } else if (length > 140) {
+      alert("Your tweet is too long!");
+    }
     const inputData = $(this).serialize();
     $.ajax('/tweets/', {
       method: 'POST',
       data: inputData
     })
-    .then(function(data) {
+    .then(function() {
+      $.ajax('/tweets/', { method: 'GET' })
+      .then(function(data) {
+        renderTweets([data[data.length - 1]]);
+      });
     });
   });
 };
@@ -60,42 +72,42 @@ const loadTweets = function() {
 };
 
 $(document).ready(function() {
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Edison",
-        "avatars": "images/profile-hex.png",
-        "handle": "@ed" },
-      "content": {
-        "text": "this is my tweet"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  // const tweetData = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Edison",
+  //       "avatars": "images/profile-hex.png",
+  //       "handle": "@ed" },
+  //     "content": {
+  //       "text": "this is my tweet"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
   
-  renderTweets(tweetData);
+  // renderTweets(tweetData);
   loadTweets();
   createTweet();
 });
